a6fd9ab26d462a6e93da2b03951762d615efc164
Dominic Tarr <dominic.tarr@gmail.com>
2018-07-23 14:37:25 -1000

    fix tests

--- test/index.js
+++ test/index.js
@@ -5,34 +5,36 @@ tape('simple', function (t) {
 
   var G = require('../')({max: 3, start: 'A'})
 
-  var count = 0, last
-  pull(G.hopStream({live: true}), pull.drain(function (e) {
-    last = e
-    count ++
-  }))
-
   var addBase = G.createLayer('base')
 
   addBase({
     A: {B: 1}
   })
 
-  t.deepEqual(G.hops(), {A: 0, B: 1})
+  var count = 0, last
+  pull(G.hopStream({live: true}), pull.drain(function (e) {
+    console.log("LIVE", e)
+    last = e
+    count ++
+  }))
+
+
+  t.deepEqual(G.getHops(), {A: 0, B: 1})
   t.equal(count, 1)
-  t.deepEqual(last, G.hops())
+  t.deepEqual(last, G.getHops())
 
   var addOver = G.createLayer('override')
 
   addOver('B', 'C', 1)
 
-  t.deepEqual(G.hops(), {A: 0, B: 1, C: 2})
+  t.deepEqual(G.getHops(), {A: 0, B: 1, C: 2})
 
   t.equal(count, 2)
   t.deepEqual(last, {C: 2})
 
   addOver('A', 'C', -1)
 
-  t.deepEqual(G.hops(), {A: 0, B: 1, C: -1})
+  t.deepEqual(G.getHops(), {A: 0, B: 1, C: -1})
 
   t.equal(count, 3)
   t.deepEqual(last, {C: -1})
@@ -40,6 +42,7 @@ tape('simple', function (t) {
 
   t.end()
 })
+
 
 
 

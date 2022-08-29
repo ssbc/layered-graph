ff464e0c637f17ab1890635b60d41c53074a94d4
Andre Staltz <andre@staltz.com>
2022-08-29 14:59:59 +0300

    tidy this package for 2022

--- test/index.js
+++ test/index.js
@@ -3,7 +3,7 @@ var pull = require('pull-stream')
 
 tape('simple', function (t) {
   var ready = false
-  var G = require('../')({max: 3, start: 'A'})
+  var G = require('../')({ max: 3, start: 'A' })
 
   var addBase = G.createLayer('base')
 
@@ -14,20 +14,23 @@ tape('simple', function (t) {
   t.equal(ready, false)
 
   addBase({
-    A: {B: 1}
+    A: { B: 1 },
   })
 
   t.equal(ready, true)
 
-  var count = 0, last
-  pull(G.hopStream({live: true}), pull.drain(function (e) {
-    console.log("LIVE", e)
-    last = e
-    count ++
-  }))
+  var count = 0,
+    last
+  pull(
+    G.hopStream({ live: true }),
+    pull.drain(function (e) {
+      console.log('LIVE', e)
+      last = e
+      count++
+    })
+  )
 
-
-  t.deepEqual(G.getHops(), {A: 0, B: 1})
+  t.deepEqual(G.getHops(), { A: 0, B: 1 })
   t.equal(count, 1)
   t.deepEqual(last, G.getHops())
 
@@ -35,29 +38,27 @@ tape('simple', function (t) {
 
   addOver('B', 'C', 1)
 
-  t.deepEqual(G.getHops(), {A: 0, B: 1, C: 2})
-  t.deepEqual(G.getHops({max:1}), {A: 0, B: 1})
-  t.deepEqual(G.getHops({reverse: true, start: 'B'}), {A: 1, B: 0})
-  t.deepEqual(G.getHops({max:4}), {A: 0, B: 1, C: 2})
+  t.deepEqual(G.getHops(), { A: 0, B: 1, C: 2 })
+  t.deepEqual(G.getHops({ max: 1 }), { A: 0, B: 1 })
+  t.deepEqual(G.getHops({ reverse: true, start: 'B' }), { A: 1, B: 0 })
+  t.deepEqual(G.getHops({ max: 4 }), { A: 0, B: 1, C: 2 })
 
   t.equal(count, 2)
-  t.deepEqual(last, {C: 2})
+  t.deepEqual(last, { C: 2 })
 
   addOver('A', 'C', -1)
 
-  t.deepEqual(G.getHops(), {A: 0, B: 1, C: -1})
+  t.deepEqual(G.getHops(), { A: 0, B: 1, C: -1 })
 
   t.equal(count, 3)
-  t.deepEqual(last, {C: -1})
-
+  t.deepEqual(last, { C: -1 })
 
   t.end()
 })
 
 tape('null causes to fall through to next layer', function (t) {
-
   var ready = false
-  var G = require('../')({max: 3, start: 'A'})
+  var G = require('../')({ max: 3, start: 'A' })
 
   var addBase = G.createLayer('base')
 
@@ -68,29 +69,28 @@ tape('null causes to fall through to next layer', function (t) {
   t.equal(ready, false)
 
   addBase({
-    A: {B: 1}
+    A: { B: 1 },
   })
 
-  t.deepEqual(G.getHops(), {A: 0, B: 1})
+  t.deepEqual(G.getHops(), { A: 0, B: 1 })
 
   var addOver = G.createLayer('override')
   addOver({})
   addOver('A', 'B', 0.1)
 
-  t.deepEqual(G.getHops(), {A: 0, B: 0.1})
+  t.deepEqual(G.getHops(), { A: 0, B: 0.1 })
   addOver('A', 'B', 1)
-  t.deepEqual(G.getHops(), {A: 0, B: 1})
+  t.deepEqual(G.getHops(), { A: 0, B: 1 })
 
   addOver('A', 'B', 2)
-  t.deepEqual(G.getHops(), {A: 0, B: 2})
+  t.deepEqual(G.getHops(), { A: 0, B: 2 })
 
   addOver('A', 'B', null)
-  t.deepEqual(G.getHops(), {A: 0, B: 1})
+  t.deepEqual(G.getHops(), { A: 0, B: 1 })
 
   addBase('A', 'B', null)
 
-  t.deepEqual(G.getHops(), {A: 0})
+  t.deepEqual(G.getHops(), { A: 0 })
 
   t.end()
 })
-
